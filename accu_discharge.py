bc81c1b12c4cb5c36cf975b75d03f59e9eb8757e
DidierA <darenzana@gmail.com>
2021-02-08 18:19:03 +0100

    grpahs on 2 views

--- accu_discharge.py
+++ accu_discharge.py
@@ -16,18 +16,25 @@ def data_gen():
     for cnt in itertools.count():
         t = cnt / 10
         # print("data_gen")
-        if (t<=7.6): # stop drawing after 7.5.  Doe snot sto the animation from runing
+        if (t<=100): 
             y1=np.sin(2*np.pi*t/5)
             y2=np.sin(2*np.pi*t/3)
-            yield t, y1, y2 # * np.exp(-t/10.)
+            yield t, y1, y2
         else:
             ani._stop() # stops the animation, private undocumented function
-            # yield -1, 0, 0 # without this and the test in data, the window is frozen after 7.5
 
 # called once. sets up graphics and data objs
 def init():
-    ax.set_ylim(-1.1, 1.1)
-    ax.set_xlim(0, 8)
+    for a in ax:
+        a.set_ylim(-1.1, 1.1)
+        a.set_xlim(0, 8)
+
+    
+    ax[0].set_ylabel("Curent (A)")
+    
+    # ax[1].set_xlablel("time")
+    ax[1].set_ylabel("Voltage (V)")
+
     del xdata[:]
     del y1data[:]
     del y2data[:]
@@ -35,10 +42,14 @@ def init():
     line2.set_data(xdata, y2data)
     return [line1,line2]
 
-fig, ax = plt.subplots()
-line1, = ax.plot([], [], lw=2)
-line2, = ax.plot([], [], lw=2)
-ax.grid()
+fig, ax = plt.subplots(2,1, sharex=True)
+
+# Remove horizontal space between axes
+fig.subplots_adjust(hspace=0)
+
+line1, = ax[0].plot([], [], lw=2)
+line2, = ax[1].plot([], [], lw=2)
+ax[0].grid(), ax[1].grid()
 xdata, y1data, y2data = [], [], []
 
 # called at each frame with the result of data_gen
@@ -46,17 +57,16 @@ def run(data):
     # update the data
     t, y1, y2 = data
     # print ('%f %f %f' % (t, y1, y2))
-    if t==-1:
-        # print ('end')
-        return 
+
     xdata.append(t)
     y1data.append(y1)
     y2data.append(y2)
-    xmin, xmax = ax.get_xlim()
+    xmin, xmax = ax[0].get_xlim()
 
     if t >= xmax:
-        ax.set_xlim(xmin, xmax+2)
-        ax.figure.canvas.draw()
+        for a in ax:
+            a.set_xlim(xmin, xmax+(xmax-xmin)/10)
+            a.figure.canvas.draw()
     
     line1.set_data(xdata, y1data)
     line2.set_data(xdata, y2data)
